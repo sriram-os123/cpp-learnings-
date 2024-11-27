e59087247a3eb037b17311e6e76a28818fc84280
sriram os123 <118521565+sriram-os123@users.noreply.github.com>
2024-11-28 00:11:30 +0530

    Create firstcode.cpp

---
+++ firstcode.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//this header relates with input and output
+using namespace std;
+
+int main(){
+    int a=50;
+    int b=6;
+    cout<<a;
+
+    cout<<"operator related adding numbers "<<a+b<<endl;
+    cout<<"operator related subtracting numbers "<<a-b<<endl;
+    cout<<"operator related multiplicating numbers "<<a*b<<endl;
+    cout<<"operator related dividing numbers "<<a/b<<endl;
+    cout<<"operator related modulo numbers "<<a%b<<endl;
+
+    float c=3.14f;
+    long double g=3.18;
+    cout<<"the given values are from different data types"<<c<<g;
